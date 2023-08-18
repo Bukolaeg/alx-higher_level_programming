158d7666fa495380cabe05807a51a057aff3c0fd
bukolaeg <bukolarongbe2015@gmail.com>
2023-08-18 06:22:41 -0700

    0x04-python-more_data_structure

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    # Create a new list to store the replaced elements
+    new_list = []
+
+    # Iterate through each element in the original list
+    for element in my_list:
+        # If the element matches the search element, replace it with the new element
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+
+    return new_list
+
