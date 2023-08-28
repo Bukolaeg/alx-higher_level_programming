88a8b20e85711861eb8f4ac02b9e09468f97ea26
bukolaeg <bukolarongbe2015@gmail.com>
2023-08-28 08:31:32 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,6 +17,9 @@ def safe_print_list_integers(my_list=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
+        except IndexError:
+            print("Index out of range.")
+            break
     print("")
-    return (ret)
+    return ret
 
